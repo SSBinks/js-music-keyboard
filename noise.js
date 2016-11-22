aadc502a23c846d23a682ba21c104a648464a3be
Shari Meggs <smeggs1@gmail.com>
2016-11-21 16:19:33 -0800

    Things prevented me from commmiting but all is well

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  // your code here
+
+
+  $('.note').on("click", function(){
+    var sound = $(this).html();
+    var noise = $('#' + sound + 'Audio')[0];
+    noise.play();
+  });
+
+
+  $('body').keydown(function(event){
+    var noise = $('#' + event.key + 'Audio')[0];
+    noise.play();
+  });
 });
