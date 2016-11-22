c49263df725947e37516b1fed520f329fe29029e
Shari Meggs <smeggs1@gmail.com>
2016-11-21 16:24:59 -0800

    added load

--- noise.js
+++ noise.js
@@ -1,15 +1,15 @@
 $(document).ready( function() {
 
-
   $('.note').on("click", function(){
     var sound = $(this).html();
     var noise = $('#' + sound + 'Audio')[0];
+    noise.load();
     noise.play();
   });
 
-
   $('body').keydown(function(event){
     var noise = $('#' + event.key + 'Audio')[0];
+    noise.load();
     noise.play();
   });
 });
