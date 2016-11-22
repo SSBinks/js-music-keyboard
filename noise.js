04c401d2ba2e9c644ba27cf99e52245fdf3fc2b3
Shari Meggs <smeggs1@gmail.com>
2016-11-21 16:46:49 -0800

    DRY

--- noise.js
+++ noise.js
@@ -1,15 +1,18 @@
+var playMusic = function(noise){
+  noise.load();
+  noise.play();
+};
+
 $(document).ready( function() {
 
   $('.note').on("click", function(){
     var sound = $(this).html();
     var noise = $('#' + sound + 'Audio')[0];
-    noise.load();
-    noise.play();
+    playMusic(noise);
   });
 
   $('body').keydown(function(event){
     var noise = $('#' + event.key + 'Audio')[0];
-    noise.load();
-    noise.play();
+    playMusic(noise);
   });
 });
